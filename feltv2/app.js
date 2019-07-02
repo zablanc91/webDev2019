d605af266e43f7a0f7d337f368e060942026ad0e
zablanc91 <zablanc91@hotmail.com>
2019-07-01 22:51:41 -0700

    first push, responsive design and navbar done

---
+++ feltv2/app.js
@@ -0,0 +1,28 @@
+
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+
+        //slide the nav back in and out of view
+        nav.classList.toggle('nav-active');
+
+        //animate nav links - utilize the index to space out the fade-in animation and then remove the animation when clicking out burger
+        navLinks.forEach( (link, index) => {
+            if(link.style.animation) {
+                link.style.animation = '';
+            }
+            else {
+                link.style.animation = `navLinkFadeIn 0.5s ease forwards ${index/5 + .35}s`;
+            }
+        });
+
+        burger.classList.toggle('burger-active');
+    });
+
+}
+
+navSlide();
